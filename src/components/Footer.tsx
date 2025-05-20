9e1fa597809cf9578994a97f4f7381328fe226e8
Ayush Shakya <ashakya406@gmail.com>
2025-05-20 23:18:48 +0545

    Made final edits in the footer.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
-import { FaInstagram, FaFacebookF, FaGithub } from "react-icons/fa";
-import { FaRegEnvelope } from "react-icons/fa6";
+import { FaInstagram, FaFacebookF, FaGithub, FaLeaf } from "react-icons/fa";
+import { FaLocationDot, FaRegEnvelope } from "react-icons/fa6";
 import { FiPhoneOutgoing } from "react-icons/fi";
 import { Montserrat_Alternates } from "next/font/google";
 
@@ -26,30 +26,32 @@ export default function Footer() {
             className="mb-4"
           />
           <p className="md:text-[16px] sm:text-sm font-normal">
-            All-in-one ecommerce platform to manage, sell, and grow—without code
+            All-in-one platform to sell farm products, connect, and grow—no
+            middlemen.
           </p>
 
           {/* Follow Us */}
           <div className="mt-15 gap-2 sm:block hidden">
             <h2
-              className={`font-semibold  text-lg mb-2 ${montserratAlt.className} text-white`}
+              className={`font-semibold  text-lg mb-1 ${montserratAlt.className} text-white`}
             >
               Follow Us
             </h2>
+            <div className="w-[80px] h-[2px] bg-[#88B04B] mb-3"></div>
             <div className="flex gap-4">
               <a href="#">
-                <button className="w-12 h-12 flex items-center justify-center bg-transparent rounded-full border border-white">
-                  <FaInstagram className="text-xl" />
+                <button className="w-10 h-10 flex items-center justify-center bg-transparent rounded-full border border-white">
+                  <FaInstagram className="text-xl hover:text-white" />
                 </button>
               </a>
               <a href="#">
-                <button className="w-12 h-12 flex items-center justify-center bg-transparent rounded-full border border-white">
-                  <FaFacebookF className="text-xl" />
+                <button className="w-10 h-10 flex items-center justify-center bg-transparent rounded-full border border-white">
+                  <FaFacebookF className="text-xl hover:text-white" />
                 </button>
               </a>
               <a href="#">
-                <button className="w-12 h-12 flex items-center justify-center bg-transparent rounded-full border border-white">
-                  <FaGithub className="text-xl" />
+                <button className="w-10 h-10 flex items-center justify-center bg-transparent rounded-full border border-white">
+                  <FaGithub className="text-xl hover:text-white" />
                 </button>
               </a>
             </div>
@@ -60,28 +62,32 @@ export default function Footer() {
         <div className=" lg:w-[40%] md:w-[48%] sm:w-[50%] flex flex-col sm:flex-row sm:gap-0 gap-8">
           <div className="flex-1 min-w-[150px]">
             <h2
-              className={`font-semibold sm:text-lg text-2xl mb-4 ${montserratAlt.className} text-white`}
+              className={`font-semibold sm:text-lg text-2xl mb-1 ${montserratAlt.className} text-white`}
             >
               Quick Links
-            </h2>{" "}
-            <div className=""></div>
+            </h2>
+            <div className="w-[90px] h-[2px] bg-[#88B04B] mb-3"></div>
             <ul className="space-y-5 sm:text-xs md:text-sm ">
-              <li>
+              <li className="flex flex-row gap-2 items-center">
+                <FaLeaf className="text-xs text-[#88B04B]" />
                 <a href="#" className="hover:underline">
                   Contact Us
                 </a>
               </li>
-              <li>
+              <li className="flex flex-row gap-2 items-center">
+                <FaLeaf className="text-xs text-[#88B04B]" />
                 <a href="#" className="hover:underline">
                   Privacy Policy
                 </a>
               </li>
-              <li>
+              <li className="flex flex-row gap-2 items-center">
+                <FaLeaf className="text-xs text-[#88B04B]" />
                 <a href="#" className="hover:underline">
                   Terms & Conditions
                 </a>
               </li>
-              <li>
+              <li className="flex flex-row gap-2 items-center">
+                <FaLeaf className="text-xs text-[#88B04B]" />
                 <a href="#" className="hover:underline">
                   About Us
                 </a>
@@ -91,17 +97,22 @@ export default function Footer() {
           {/* Get in Touch */}
           <div className="flex-1 min-w-[200px]">
             <h2
-              className={`font-semibold sm:text-lg text-2xl mb-4 ${montserratAlt.className} text-white`}
+              className={`font-semibold sm:text-lg text-2xl mb-1 ${montserratAlt.className} text-white`}
             >
               Get in Touch
             </h2>
+            <div className="w-[90px] h-[2px] bg-[#88B04B] mb-3"></div>
             <div className="flex items-start gap-3 mb-5 md:text-sm sm:text-xs">
               <FaRegEnvelope className="text-xl mt-[2px]" />
-              <span>support@loopscart.com</span>
+              <span>support@annapur.com</span>
             </div>
-            <div className="flex items-start gap-3 md:text-sm sm:text-xs">
+            <div className="flex items-start gap-3 mb-5 md:text-sm sm:text-xs">
               <FiPhoneOutgoing className="text-xl mt-[2px]" />
               <span>+977 982-3821451</span>
+            </div>
+            <div className="flex items-start gap-3 md:text-sm sm:text-xs">
+              <FaLocationDot className="text-xl mt-[2px]" />
+              <span>Lalitpur-16, Nepal</span>
             </div>
           </div>
 
@@ -134,7 +145,10 @@ export default function Footer() {
       </div>
       <div className="flex flex-row items-center justify-around w-full h-15 mt-10 text-center text-sm bg-[#1F1E17]">
         <div>
-          <p>COPYRIGHT © 2025 LoopsCart E-commerce | All Rights Reserved</p>
+          <p>
+            COPYRIGHT © 2025 Annapur - Farmer&apos;s Market | All Rights
+            Reserved
+          </p>
         </div>
       </div>
     </footer>
