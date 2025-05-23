b1a7ecf1670f546c16062f1e4b79d76670257690
Ayush Shakya <ashakya406@gmail.com>
2025-05-23 10:25:39 +0545

    ...nothing much changed.

--- src/components/Explore.tsx
+++ src/components/Explore.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import FeatureCard from "./FeatureCard";
 import HeaderText from "./HeaderText";
-import { FaHandshake, FaShoppingBasket, FaShoppingCart} from "react-icons/fa";
+import { FaHandshake, FaShoppingBasket, FaShoppingCart } from "react-icons/fa";
 import { GiFarmer } from "react-icons/gi";
 import { FaLocationDot } from "react-icons/fa6";
 import { PiGavelFill } from "react-icons/pi";
@@ -26,7 +26,7 @@ const Explore = () => {
             />
           </div>
           <div className="w-[480px]">
-                      {/* <p className=" text-[16px] font-medium text-[#878680] text-left  mx-auto mb-3">
+            {/* <p className=" text-[16px] font-medium text-[#878680] text-left  mx-auto mb-3">
               There are many variations of passenger of available but the
               majority have suffered alteration in some form by injected hutter
               or random weed which don&apos;t look even.
@@ -36,9 +36,7 @@ const Explore = () => {
               {/* Feature 1*/}
 
               <FeatureCard
-                icon={
-                <GiFarmer className="text-[#6f963a] mt-7 h-7 w-7" />
-            }
+                icon={<GiFarmer className="text-[#6f963a] mt-7 h-7 w-7" />}
                 text="Direct From Farmers"
                 text2="Skip the middlemen and get fresh produce straight from the source - your
                 local farmers."
@@ -47,8 +45,8 @@ const Explore = () => {
               {/* Feature 2 */}
               <FeatureCard
                 icon={
-                <FaShoppingCart className="text-[#88B04B] mt-7 h-7 w-7" />
-            }
+                  <FaShoppingCart className="text-[#88B04B] mt-7 h-7 w-7" />
+                }
                 text="Fair prices for Everyone"
                 text2="Farmers earn what they truly deserve and buyers enjoy honest,
                 transparent pricing."
@@ -57,7 +55,7 @@ const Explore = () => {
               {/* Feature 3 */}
               <FeatureCard
                 icon={
-                <FaLocationDot className="text-[#A9BA56] mt-7 h-7 w-7 " />
+                  <FaLocationDot className="text-[#A9BA56] mt-7 h-7 w-7 " />
                 }
                 text="Location-Based Access"
                 text2="Use smart maps to discover and buy from nearby farmers with ease 
@@ -66,7 +64,7 @@ const Explore = () => {
               {/* Feature 4 */}
               <FeatureCard
                 icon={
-                <FaShoppingBasket className="text-[#C5CE38] mt-7 h-7 w-7 " />
+                  <FaShoppingBasket className="text-[#C5CE38] mt-7 h-7 w-7 " />
                 }
                 text="Organic and Logically grown"
                 text2="Find safe, natural and organic products that support your health
@@ -74,18 +72,14 @@ const Explore = () => {
               />
               {/* Feature 5 */}
               <FeatureCard
-                icon={
-                <PiGavelFill className="text-[#D8D665] mt-7 h-7 w-7 " />
-                }
+                icon={<PiGavelFill className="text-[#D8D665] mt-7 h-7 w-7 " />}
                 text="Smart Bidding System"
                 text2="Competitive yet fair- Bid on the produce you want while farmers
                 pick the best offer."
               />
               {/* Feature 6 */}
-              <FeatureCard 
-                icon={
-                <FaHandshake className="text-[#EEDC87] mt-7 h-7 w-7 " />
-                }
+              <FeatureCard
+                icon={<FaHandshake className="text-[#EEDC87] mt-7 h-7 w-7 " />}
                 text="Trusted Community Network"
                 text2="Annapur connects concious buyers and hardworking farmers in a
                 reliable, growing marketplace."
