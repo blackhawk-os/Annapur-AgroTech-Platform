efa6d3fe68cc8d2876eeef3ab44eb37ad7427518
Ayush Shakya <ashakya406@gmail.com>
2025-05-25 15:57:53 +0545

    Added hover effect on faq section

--- src/components/Faqcard.tsx
+++ src/components/Faqcard.tsx
@@ -1,40 +1,41 @@
 "use client";
 
-import React, { useState } from 'react';
-import { FaChevronRight} from 'react-icons/fa';
+import React, { useState } from "react";
+import { FaChevronRight } from "react-icons/fa";
 
 interface FAQCardProps {
-    question: string;
-    answer: string;
+  question: string;
+  answer: string;
 }
 
 export const FAQCard = ({ question, answer }: FAQCardProps) => {
-    const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-    return (
-        <div className="w-full border-b border-gray-200 pb-4 mb-4 last:border-0 last:mb-0">
-            <button
-                onClick={() => setIsOpen(!isOpen)}
-                className=" flex items-center gap-3 w-full text-left focus:outline-none"
-            >
-
-                <span className={`transition-transform duration-300 ${isOpen ? "rotate-90" : ""}`}>
-
-                    <FaChevronRight strokeWidth={1} className="w-5 h-5 text-gray-800"
-                    />
-                </span>
-                <span className="text-lg font-medium text-gray-800">
-                    {question}
-                </span>
-
-            </button>
-
-            <div 
-            className={`overflow-hidden transition-all duration-200 pl-9 ${
-                isOpen ? 'max-h-96 mt-3' : 'max-h-0' }`}
-             >
-                <p className=" text-gray-600 pb-2">{answer}</p>
-             </div>
+  return (
+    <div className="w-full border-b border-gray-200 pb-4 mb-4 last:border-0 last:mb-0">
+      <button
+        onClick={() => setIsOpen(!isOpen)}
+        className=" flex items-center w-full text-left focus:outline-none"
+      >
+        <div className="flex flex-row justify-center align-center items-center gap-3 text-gray-800 hover:text-[#88b04b]">
+          <span
+            className={`transition-transform duration-300   ${
+              isOpen ? "rotate-90" : ""
+            }`}
+          >
+            <FaChevronRight strokeWidth={1} className="w-5 h-5" />
+          </span>
+          <span className="text-lg font-medium ">{question}</span>
         </div>
-    );
+      </button>
+
+      <div
+        className={`overflow-hidden transition-all duration-200 pl-9 ${
+          isOpen ? "max-h-96 mt-3" : "max-h-0"
+        }`}
+      >
+        <p className=" text-gray-400 pb-2 font-normal">{answer}</p>
+      </div>
+    </div>
+  );
 };
