87b66a4b3f6adc20b52bc914bbab6cc28ae8d6ab
Ayush Shakya <ashakya406@gmail.com>
2025-05-26 08:03:44 +0545

    Added about us , contact us , market and account pages.

--- src/components/MarqueeCard.tsx
+++ src/components/MarqueeCard.tsx
@@ -1,16 +1,16 @@
 "use client";
 
-import React from 'react';
+import React from "react";
 
-interface MarqueeCardProps{
-    icon: React.ReactNode;
-    title: string;
-    tagline?: string;
+interface MarqueeCardProps {
+  icon: React.ReactNode;
+  title: string;
+  tagline?: string;
 }
 
 export const MarqueeCard = ({ icon, title, tagline }: MarqueeCardProps) => {
-    return (
-        <div className="flex items-center gap-4 px-8 py-4 bg-white rounded-lg shadow-md mx-4 min-w-[300px]">
+  return (
+    <div className="flex items-center gap-4 px-8 py-4 bg-white rounded-lg shadow-md mx-4 min-w-[300px]">
       <div className="text-[#88B04B] text-3xl">{icon}</div>
       <div>
         <h3 className="text-xl font-semibold text-gray-800">{title}</h3>
