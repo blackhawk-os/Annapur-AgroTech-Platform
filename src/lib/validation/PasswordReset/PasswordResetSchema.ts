1390afea37558e7e0757d28067cef65683561c6a
Mehul Gopal Nyachhyon <aventmehul@gmail.com>
2025-06-15 15:21:19 +0545

    Feature: Added OTP after reset password

--- src/lib/validation/PasswordReset/PasswordResetSchema.ts
+++ src/lib/validation/PasswordReset/PasswordResetSchema.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 
 export const forgotPasswordSchema = z.object({
   email: z.string().min(1, "Email is required").email("Invalid email address"),
+  otp: z.string().optional(),
 });
 
 export type ForgotPasswordInput = z.infer<typeof forgotPasswordSchema>;
